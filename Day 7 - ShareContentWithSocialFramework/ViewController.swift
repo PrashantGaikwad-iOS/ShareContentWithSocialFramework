4e34f35d61da885b9608f66bc334f8f6ea428dac
Prashant Gaikwad <prashant.gaikwad@leftrightmind.com>
2018-04-02 07:47:23 +0530

    First commit

--- Day 7 - ShareContentWithSocialFramework/ViewController.swift
+++ Day 7 - ShareContentWithSocialFramework/ViewController.swift
@@ -7,6 +7,14 @@
 //
 
 import UIKit
+import Social
+
+/*
+ 
+ Twitter : Share With Image,Content and URL.
+ Facebook :  Share With Image,Content and URL.
+ 
+ */
 
 class ViewController: UIViewController {
 
@@ -18,6 +26,36 @@ class ViewController: UIViewController {
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
+  }
+  
+  @IBAction func tweetButtonShare(_ sender: Any) {
+    if SLComposeViewController.isAvailable(forServiceType: SLServiceTypeTwitter) {
+      
+      let tweetShare = SLComposeViewController(forServiceType: SLServiceTypeTwitter)
+      if let tweetShare = tweetShare {
+        tweetShare.setInitialText("Nice Tutorials of iOSDevCenters")
+        tweetShare.add(UIImage(named: "iOSDevCenters.jpg")!)
+        tweetShare.add(URL(string: "https://iosdevcenters.blogspot.com"))
+        self.present(tweetShare, animated: true, completion: nil)
+      }
+    } else {
+      print("Not Available")
+    }
+  }
+  
+  @IBAction func facebookButtonShare(_ sender: Any) {
+    if SLComposeViewController.isAvailable(forServiceType: SLServiceTypeFacebook) {
+      
+      let facebookShare = SLComposeViewController(forServiceType: SLServiceTypeFacebook)
+      if let facebookShare = facebookShare{
+        facebookShare.setInitialText("Nice Tutorials of iOSDevCenters")
+        facebookShare.add(UIImage(named: "iOSDevCenters.jpg")!)
+        facebookShare.add(URL(string: "https://iosdevcenters.blogspot.com"))
+        self.present(facebookShare, animated: true, completion: nil)
+      }
+    } else {
+      print("Not Available")
+    }
   }
 
 
